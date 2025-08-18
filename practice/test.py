b9b02fbdf793351f4c45c6462678fa6bf3b0433d
ruturaj264 <ruturajdeshmukh0@gmail.com>
2025-08-18 13:19:13 +0530

    program to reverse list added in test.py

--- practice/test.py
+++ practice/test.py
@@ -10,18 +10,33 @@
 #     print('prime')
 
 
-mylist = [2,8,3,1,9]
-n = len(mylist)
+# mylist = [2,8,3,1,9]
+# n = len(mylist)
 
 
-for i in range(n-1):
+# for i in range(n-1):
 
-    for j in range(i+1, n):
+#     for j in range(i+1, n):
 
-        if mylist[i] > mylist[j]:
-            mylist[i], mylist[j] = mylist[j], mylist[i]
+#         if mylist[i] > mylist[j]:
+#             mylist[i], mylist[j] = mylist[j], mylist[i]
 
-print(mylist)
+# print(mylist)
+
+# my_list = [1,2,3,4,5,6]
+
+# l = len(my_list)
+# n = l // 2
+
+# for i in range(n):
+
+#     index1 = i
+#     index2 = l - (i+1)
+
+#     my_list[index1], my_list[index2] = my_list[index2], my_list[index1]
+
+
+# print(my_list)
 
 
 
